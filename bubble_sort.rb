5ae408d48690c6c5f3fce4444d5410b2c06bbce7
Muhammad Tashfeen <raotashfeen25@gmail.com>
2019-12-09 18:25:38 -0600

    rbocop is added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,33 +1,34 @@
-def bubble_sort(arr)
+# frozen_string_literal: true
 
+def bubble_sort(arr)
   n = arr.length
   loop do
     swapped = false
-    (n-1).times do |k|
+    (n - 1).times do |k|
       if arr[k] > arr[k + 1]
         arr[k], arr[k + 1] = arr[k + 1], arr[k]
         swapped = true
       end
     end
-    break if !swapped
+    break unless swapped
   end
   print arr
 end
 
-def bubble_sort_by (arr)
-  arr.each do |x|
-    arr.each_with_index do |value,key|
-      if key != (arr.length()-1)
-        if yield(value, arr[key+1]) > 0
-          arr[key],arr[key+1] = arr[key+1],arr[key]
-        end
+def bubble_sort_by(arr)
+  arr.each do
+    arr.each_with_index do |value, key|
+      next unless key != (arr.length - 1)
+
+      if yield(value, arr[key + 1]).positive?
+        arr[key], arr[key + 1] = arr[key + 1], arr[key]
       end
     end
   end
-   arr
+  arr
 end
 
-arr = [3,5,6,9,2,3]
-string = ["hey","hello","hi"]
-p bubble_sort_by(string){|a,b| b.length - a.length}
+arr = [3, 5, 6, 9, 2, 3]
+string = %w[hey hello hi]
+p bubble_sort_by(string) { |a, b| b.length - a.length }
 p bubble_sort(arr)
